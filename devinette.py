ef67db48d5b52d06d5142945a46f744bc4e1826d
Adel Noureddine <adelnoureddine@users.noreply.github.com>
2025-10-16 14:35:34 +0200

    V1 de la devinette

---
+++ devinette.py
@@ -0,0 +1,10 @@
+print("Voici la devinette")
+
+nombre_secret = 23
+
+nombre_utilisateur = input("Taper un nombre entre 0 et 100")
+
+if nombre_utilisateur == nombre_secret:
+    print("Gagné")
+else:
+    print("Perdu")
