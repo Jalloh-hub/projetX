6bfa7ee77ee5a07e1beb4fd5e8945641fa034739
Misasoa Robison <125265771+MisasoaRobison@users.noreply.github.com>
2025-10-16 15:36:24 +0200

    Modification de devinette.py pour pouvoir redemander le nombre si échec

--- devinette.py
+++ devinette.py
@@ -7,4 +7,10 @@ nombre_utilisateur = int(input("Taper un nombre entre 0 et 100"))
 if nombre_utilisateur == nombre_secret:
     print("Gagné")
 else:
-    print("Perdu")
+    print("Perdu. Mais ce n'est pas grave, tu as droit à une deuxieme chance !!")
+    nombre_utilisateur = int(input("Dernière chane, chosis ton nombre : "))
+    if nombre_utilisateur == nombre_secret :
+        print("Gagné")
+    else:
+        print("Dommage, tu as encore perdu")
+print("Fin")
